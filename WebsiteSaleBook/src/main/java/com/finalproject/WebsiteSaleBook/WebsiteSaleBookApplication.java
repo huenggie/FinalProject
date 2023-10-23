87c5b67b528a8f23b3ed462a98c4bf2d8f1f680b
huenggie <ddoramon.zzz@gmail.com>
2023-10-23 13:29:57 +0700

    FE 1

---
+++ WebsiteSaleBook/src/main/java/com/finalproject/WebsiteSaleBook/WebsiteSaleBookApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.WebsiteSaleBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteSaleBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteSaleBookApplication.class, args);
+	}
+
+}
